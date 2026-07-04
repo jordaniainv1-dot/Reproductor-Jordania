3484dc4e18a9490e5fca6a61bb9824bdba05db2f
inventario jh <jordaniainv1@gmail.com>
2026-07-04 15:16:10 -0500

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,23 @@
+/*
+  ============================================================
+  CONFIGURACIÓN DE FIREBASE — MEGÁFONO
+  ============================================================
+  Este archivo SOLO contiene la configuración de Firebase.
+  NO inicializa Firebase. Esa tarea la realiza
+  megafono-firebase.js
+  ============================================================
+*/
+
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyD_txrS3JN_heKGUB7kxwNXexoWYlE0R6k",
+  authDomain: "reproductor-de-jordania.firebaseapp.com",
+  projectId: "reproductor-de-jordania",
+  storageBucket: "reproductor-de-jordania.firebasestorage.app",
+  messagingSenderId: "798069373236",
+  appId: "1:798069373236:web:c3d7e743165e53e1764d61"
+};
+
+/*
+  PIN del panel de control
+*/
+window.CONTROL_PIN = "1234";
